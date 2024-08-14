24ce46f9a3b5e6d75ca9b2f19e7b4918bf584591
Ankur Vasani <ankurvasani2@gmail.com>
2024-08-14 20:32:51 +0530

    pillow

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from inference_sdk import InferenceHTTPClient, InferenceConfiguration
+from PIL import Image
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
@@ -11,6 +12,11 @@ CLIENT = InferenceHTTPClient(
     api_key="vlb86oyC1Fuerrheol9q"
 )
 
+
+def compress_image(input_path, output_path, max_size=(1024, 1024), quality=85):
+    with Image.open(input_path) as img:
+        img.thumbnail(max_size)
+        img.save(output_path, "JPEG", quality=quality)
 @app.route('/upload', methods=["POST"])
 def upload():
     if request.method == "POST":
@@ -19,7 +25,7 @@ def upload():
 
         # Save the uploaded image
         imagefile.save(filename)
-
+        compress_image(filename, filename)
         try:
             # Infer using the InferenceHTTPClient
             with CLIENT.use_configuration(custom_configuration):
